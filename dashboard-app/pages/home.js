e2061705c4bf4b2db67dee7095a9dee4c39f14c0
Niels GEORGE-PEILLEX <niels.george-peillex@epitech.eu>
2019-11-13 14:48:54 +0100

    feat: Creating of a right bar to choose services and widgets to display.

--- dashboard-app/pages/home.js
+++ dashboard-app/pages/home.js
@@ -7,13 +7,21 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import SettingsIcon from '@material-ui/icons/Settings';
+import { Drawer } from '@material-ui/core';
+import { List } from '@material-ui/core';
+import { ListItem } from '@material-ui/core';
+import { ListItemIcon } from '@material-ui/core';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import Divider from '@material-ui/core/Divider';
 
 const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
     },
     toolbar: {
-        paddingRight: 24, // keep right padding when drawer closed
+        paddingRight: 24,
     },
     appBar: {
         zIndex: theme.zIndex.drawer + 1,
@@ -25,24 +33,78 @@ const useStyles = makeStyles(theme => ({
     title: {
         flexGrow: 1,
     },
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
 }));
 
 export default function Dashboard() {
     const classes = useStyles();
 
+    const [state, setState] = React.useState({
+        top: false,
+        left: false,
+        bottom: false,
+        right: false,
+    });
+
+    const toggleDrawer = (side, open) => event => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [side]: open });
+    };
+
+    const sideList = side => (
+        <div
+            className={classes.list}
+            role="presentation"
+            onClick={toggleDrawer(side, false)}
+            onKeyDown={toggleDrawer(side, false)}
+        >
+            <List>
+                {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                    <ListItem button key={text}>
+                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+            <Divider />
+        </div>
+    );
+
+    const NavigationBar = () => (
+        <AppBar position="absolute" className={clsx(classes.appBar)}>
+            <Toolbar className={classes.toolbar}>
+                <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                    Dashboard
+          </Typography>
+                <IconButton color="inherit" onClick={toggleDrawer('right', true)}>
+                    <SettingsIcon />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+    );
+
+    const RightNavBar = () => (
+        <Drawer anchor="right" open={state.right} onClose={toggleDrawer('right', false)}>
+            {sideList('right')}
+        </Drawer>
+    )
+
     return (
         <div className={classes.root}>
             <CssBaseline />
-            <AppBar position="absolute" className={clsx(classes.appBar)}>
-                <Toolbar className={classes.toolbar}>
-                    <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-                        Dashboard
-          </Typography>
-                    <IconButton color="inherit">
-                        <SettingsIcon />
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
+            <NavigationBar />
+
+            <RightNavBar />
         </div>
     );
 }
